9fb60b0f135463ced2227be3a3b0c3febd0c41d3
MichaelNgCen <michaelngcen@yahoo.com>
2023-06-03 00:46:03 -0400

    first commit

---
+++ components/Sidebar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface SidebarProps {
+  children: React.ReactNode;
+}
+
+const Sidebar: React.FC<SidebarProps> = ({
+    children,
+}) => {
+  return <div></div>;
+};
+
+export default Sidebar;
