54f1c93e985bd68ed822db4abb37f816d408d555
MichaelNgCen <michaelngcen@yahoo.com>
2023-06-04 01:46:41 -0400

    add 2 buttons

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,3 +1,11 @@
+import Header from "@/components/Header";
+
 export default function Home() {
-  return <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto"> HELLO (Site) </div>;
+  return (
+    <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
+      <Header>
+        Header
+      </Header>
+    </div>
+  );
 }
