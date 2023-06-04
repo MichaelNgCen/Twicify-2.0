ee03b1b50b70a914bb8c4ac1ecdde417faefa704
MichaelNgCen <michaelngcen@yahoo.com>
2023-06-04 02:03:02 -0400

    add buttons for mobile

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,6 +4,8 @@ import React from "react";
 import { useRouter } from "next/navigation";
 import { twMerge } from "tailwind-merge";
 import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
+import { HiHome } from "react-icons/hi";
+import { BiSearch } from "react-icons/bi";
 
 interface HeaderProps {
   children: React.ReactNode;
@@ -23,11 +25,25 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
     >
       <div className="w-full mb-4 flex items-center justify-between">
         <div className="hidden md:flex gap-x-2 items-center">
-          <button onClick={() => router.back()} className="rounded-full bg-black flex items-center justify-center hover:opacity-75 transition">
+          <button
+            onClick={() => router.back()}
+            className="rounded-full bg-black flex items-center justify-center hover:opacity-75 transition"
+          >
             <RxCaretLeft className="text-white" size={35}></RxCaretLeft>
           </button>
-          <button onClick={() => router.forward} className="rounded-full bg-black flex items-center justify-center hover:opacity-75 transition">
+          <button
+            onClick={() => router.forward}
+            className="rounded-full bg-black flex items-center justify-center hover:opacity-75 transition"
+          >
             <RxCaretRight className="text-white" size={35}></RxCaretRight>
+          </button>
+        </div>
+        <div className="flex md:hidden gap-x-2 items-center">
+          <button className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition">
+            <HiHome className="text-black" size={20} />
+          </button>
+          <button className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition">
+            <BiSearch className="text-black" size={20} />
           </button>
         </div>
       </div>
