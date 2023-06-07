d16e82853bda3c62d9ea8151b217cea4dc84dbd2
MichaelNgCen <michaelngcen@yahoo.com>
2023-06-07 00:11:06 -0400

    add for types

--- types.ts
+++ types.ts
@@ -9,3 +9,46 @@ export interface UserDetails {
     billing_address?: Stripe.Address;
     payment_methods?: Stripe.PaymentMethod[Stripe.PaymentMethod.Type];
 }
+
+export interface Product {
+    id: string;
+    active?: boolean;
+    description?: string;
+    name?: string;
+    images?: string;
+    metadata?: Stripe.Metadata;
+}
+
+export interface Price {
+    id: string;
+    product_id?: string;
+    active?: boolean;
+    description?: string;
+    unit_amount?: number;
+    currency?: string;
+    type?: Stripe.Price.Type;
+    interval?: Stripe.Price.Recurring.Interval;
+    interval_count?: number;
+    trial_period_days?: number;
+    metadata?: Stripe.Metadata;
+    products?: Product;
+}
+
+export interface Subscription {
+    id: string;
+    user_id: string;
+    status?: Stripe.Subscription.Status;
+    metadata?: Stripe.Metadata;
+    price_id?: string;
+    quantity?: number;
+    cancel_at_period_end?: boolean;
+    created: string
+    current_period_end: string
+    current_period_start: string
+    ended_at?: string;
+    cancel_at?: string;
+    canceled_at?: string;
+    trial_end?: string;
+    trial_start?: string;
+    prices?: Price;
+}
