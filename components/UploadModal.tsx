1c3fa015358cfb903dd241fc6d58b7ee4ecf6586
MichaelNgCen <michaelngcen@yahoo.com>
2023-06-08 14:43:19 -0400

    Upload modal

--- components/UploadModal.tsx
+++ components/UploadModal.tsx
@@ -1,12 +1,27 @@
-import React from "react"
+"use client";
+
+import React from "react";
 import Modal from "./Modal";
+import useUploadModal from "@/app/hooks/useUploadModal";
 
 const UploadModal = () => {
+  const uploadModal = useUploadModal();
+
+  const onChange = () => {
+    if (!open) {
+      uploadModal.onClose();
+    }
+  };
   return (
-    <Modal title="Upload Modal Title" description="Upload Modal Description" isOpen onChange={() => {}}>
-        Upload Content
+    <Modal
+      title="Upload Modal Title"
+      description="Upload Modal Description"
+      isOpen={uploadModal.isOpen}
+      onChange={onChange}
+    >
+      Upload Content
     </Modal>
-  )
+  );
 };
 
 export default UploadModal;
