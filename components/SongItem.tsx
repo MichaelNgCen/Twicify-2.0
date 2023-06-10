3a6eaef8c7cfe6864f8de5720d6cbbd670f7d1a6
MichaelNgCen <michaelngcen@yahoo.com>
2023-06-10 01:33:17 -0400

    fetch song title and author

--- components/SongItem.tsx
+++ components/SongItem.tsx
@@ -1,29 +1,38 @@
-'use client';
+"use client";
 
 import Image from "next/image";
 import useLoadImage from "@/app/hooks/useLoadImage";
 import { Song } from "@/types";
-import React from "react"
+import React from "react";
 
 interface SongItemProps {
-    data: Song;
-    onClick: (id: string) => void;
+  data: Song;
+  onClick: (id: string) => void;
 }
 
-const SongItem: React.FC<SongItemProps> = ({
-    data,
-    onClick
-}) => {
-    const imagePath = useLoadImage(data)
+const SongItem: React.FC<SongItemProps> = ({ data, onClick }) => {
+  const imagePath = useLoadImage(data);
   return (
-    <div onClick={() => onClick(data.id)} className="relative group flex flex-col items-center justify-center rounded-md overflow-hidden gap-x-4 cursor-pointer hover:bg-neutral-400/10 transition p-3 bg-neutral-400/5 ">
-        <div className="relative aspect-square w-full h-full rounded-md overflow-hidden">
-            <Image
-            className="object-cover" src={imagePath || '/image/liked.png'} fill alt="Image"
-            />
-        </div>
+    <div
+      onClick={() => onClick(data.id)}
+      className="relative group flex flex-col items-center justify-center rounded-md overflow-hidden gap-x-4 cursor-pointer hover:bg-neutral-400/10 transition p-3 bg-neutral-400/5 "
+    >
+      <div className="relative aspect-square w-full h-full rounded-md overflow-hidden">
+        <Image
+          className="object-cover"
+          src={imagePath || "/image/liked.png"}
+          fill
+          alt="Image"
+        />
+      </div>
+      <div className="flex flex-col items-start w-full pt-4 gap-y-1">
+        <p className="font-semibold truncate w-full">{data.title}</p>
+        <p className="text-neutral-400 text-sm pb-4 w-full truncate">
+          By {data.author}
+        </p>
+      </div>
     </div>
-  )
+  );
 };
 
 export default SongItem;
