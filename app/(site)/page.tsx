f020da7ef5b28f19062e7d96a948cbbd2d37cdfa
MichaelNgCen <michaelngcen@yahoo.com>
2023-06-09 23:17:16 -0400

    fetch song

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,9 +1,11 @@
+import getSongs from "@/actions/getSongs";
 import Header from "@/components/Header";
 import ListItem from "@/components/ListItem";
 
 export const revalidate = 0;
 
 export default async function Home() {
+  const songs = await getSongs();
   return (
     <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
       <Header>
@@ -22,7 +24,11 @@ export default async function Home() {
         <div className="flex justify-between items-center">
           <h1 className="text-white text-2xl font-semibold">Newest songs</h1>
         </div>
-        <div>List of Songs!</div>
+        <div>
+          {songs.map((song) => (
+            <div> {song.title} </div>
+          ))}
+        </div>
       </div>
     </div>
   );
