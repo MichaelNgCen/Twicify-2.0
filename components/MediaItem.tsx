cf2985ee67d67a736ae7ba8928d685e8911a6a3a
MichaelNgCen <michaelngcen@yahoo.com>
2023-06-10 02:47:27 -0400

    add song and author to sidebar

--- components/MediaItem.tsx
+++ components/MediaItem.tsx
@@ -21,7 +21,7 @@ const MediaItem: React.FC<MediaItemProps> = ({ data, onClick }) => {
   return (
     <div
       onClick={handleClick}
-      className="flex items-centergap-x-3 cursor-pointer hover:bg-neutral-800/50 w-full p-2 rounded-md"
+      className="flex items-center gap-x-3 cursor-pointer hover:bg-neutral-800/50 w-full p-2 rounded-md"
     >
       <div className="relative rounded-md min-h-[48px] min-w-[48px] overflow-hidden">
         <Image
@@ -30,6 +30,10 @@ const MediaItem: React.FC<MediaItemProps> = ({ data, onClick }) => {
           alt="Media Item"
           className="object-cover"
         />
+      </div>
+      <div className="flex flex-col gap-y-1 overflow-hidden">
+        <p className="text-white truncate">{data.title}</p>
+        <p className="text-neutral-400 text-sm truncate">{data.author}</p>
       </div>
     </div>
   );
