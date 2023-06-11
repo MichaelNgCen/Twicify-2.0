09854a35cd84f2e7aff9b924b0e83f4c2ff77946
MichaelNgCen <michaelngcen@yahoo.com>
2023-06-11 04:15:04 -0400

    fetch liked songs

--- app/liked/components/LikedContent.tsx
+++ app/liked/components/LikedContent.tsx
@@ -1,9 +1,11 @@
-'use client'
+"use client";
 
 import { Song } from "@/types";
 import React, { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { useUser } from "@/app/hooks/useUser";
+import MediaItem from "@/components/MediaItem";
+import LikeButton from "@/components/LikeButton";
 
 interface LikedContentProps {
   songs: Song[];
@@ -27,7 +29,18 @@ const LikedContent: React.FC<LikedContentProps> = ({ songs }) => {
     );
   }
 
-  return <div>LikedContent!</div>;
+  return (
+    <div className="flex flex-col gap-y-2 w-full p-6">
+      {songs.map((song) => (
+        <div key={song.id} className="flex items-center gap-x-4 w-full">
+          <div className="flex-1">
+            <MediaItem onClick={() => {}} data={song} />
+          </div>
+          <LikeButton songId={song.id} />
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default LikedContent;
