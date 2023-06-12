bc03da542f8b72069d3579cdcbec8ea883ab71c4
MichaelNgCen <michaelngcen@yahoo.com>
2023-06-12 00:46:31 -0400

    player hook

--- components/Player.tsx
+++ components/Player.tsx
@@ -1,19 +1,17 @@
-'use client';
+"use client";
 
 import useGetSongsById from "@/app/hooks/useGetSongById";
+import useLoadSongUrl from "@/app/hooks/useLoadSongUrl";
 import usePlayer from "@/app/hooks/usePlayer";
-import React from "react"
+import React from "react";
 
 const Player = () => {
-    const player = usePlayer();
-    const { song } = useGetSongsById(player.activeId);
+  const player = usePlayer();
+  const { song } = useGetSongsById(player.activeId);
 
+  const songUrl = useLoadSongUrl;
 
-  return (
-    <div>
-      PLAYER
-    </div>
-  )
+  return <div>PLAYER</div>;
 };
 
 export default Player;
