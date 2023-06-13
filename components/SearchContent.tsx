ad84f0d3d4bf7d8804f90adf18c7351454b03084
MichaelNgCen <michaelngcen@yahoo.com>
2023-06-13 00:04:58 -0400

    add play to search

--- components/SearchContent.tsx
+++ components/SearchContent.tsx
@@ -4,12 +4,14 @@ import { Song } from "@/types";
 import React from "react";
 import MediaItem from "./MediaItem";
 import LikeButton from "./LikeButton";
+import useOnPlay from "@/app/hooks/useOnPlay";
 
 interface SearchContentProps {
   songs: Song[];
 }
 
 const SearchContent: React.FC<SearchContentProps> = ({ songs }) => {
+  const onPlay = useOnPlay(songs);
   if (songs.length === 0) {
     return (
       <div className="flex flex-col gap-y-2 w-full px-6 text-neutral-400">
@@ -22,7 +24,7 @@ const SearchContent: React.FC<SearchContentProps> = ({ songs }) => {
       {songs.map((song: Song) => (
         <div key={song.id} className="flex items-center gap-x-4 w-full">
           <div className="flex-1">
-            <MediaItem onClick={() => {}} data={song} />
+            <MediaItem onClick={(id: string) => onPlay(id)} data={song} />
           </div>
           <LikeButton songId={song.id} />
         </div>
