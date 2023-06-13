0e8b832430ccd5daadf4fc488f3cdfb4c7f3bef3
MichaelNgCen <michaelngcen@yahoo.com>
2023-06-12 23:57:47 -0400

    sidebar play

--- components/PlayerContent.tsx
+++ components/PlayerContent.tsx
@@ -95,7 +95,7 @@ const PlayerContent: React.FC<PlayerContentProps> = ({ song, songUrl }) => {
       </div>
       <div className="flex md:hidden col-auto justify-end items-center">
         <div
-          onClick={() => {}}
+          onClick={handlePlay}
           className="h-10 w-10 flex items-center justify-center rounded-full bg-white p-1 cursor-pointer"
         >
           <Icon size={30} className="text-black" />
@@ -108,7 +108,7 @@ const PlayerContent: React.FC<PlayerContentProps> = ({ song, songUrl }) => {
           className="text-neutral-400"
         />
         <div
-          onClick={() => {}}
+          onClick={handlePlay}
           className="flex items-center justify-center h-10 w-10 rounded-full bg-white p-1 cursor-pointer"
         >
           <Icon size={30} className="text-black" />
