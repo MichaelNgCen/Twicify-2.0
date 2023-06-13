5ae688e32334b88f4d05b8d72db162d1d1a5c850
MichaelNgCen <michaelngcen@yahoo.com>
2023-06-13 01:24:43 -0400

    start account page

---
+++ app/account/page.tsx
@@ -0,0 +1,11 @@
+import Header from "@/components/Header";
+
+const Account = () => {
+  return (
+    <div className=" bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
+      Account
+    </div>
+  );
+};
+
+export default Account;
