c2198260d5b6f9549fef3771b3f88c8bec9d1d22
MichaelNgCen <michaelngcen@yahoo.com>
2023-06-13 00:07:16 -0400

    add play to liked

--- app/liked/components/LikedContent.tsx
+++ app/liked/components/LikedContent.tsx
@@ -6,6 +6,7 @@ import { useRouter } from "next/navigation";
 import { useUser } from "@/app/hooks/useUser";
 import MediaItem from "@/components/MediaItem";
 import LikeButton from "@/components/LikeButton";
+import useOnPlay from "@/app/hooks/useOnPlay";
 
 interface LikedContentProps {
   songs: Song[];
@@ -21,6 +22,8 @@ const LikedContent: React.FC<LikedContentProps> = ({ songs }) => {
     }
   }, [isLoading, user, router]);
 
+  const onPlay = useOnPlay(songs);
+
   if (songs.length === 0) {
     return (
       <div className="flex flex-col gap-y-2 w-full px-6 text-neutral-400">
@@ -34,7 +37,7 @@ const LikedContent: React.FC<LikedContentProps> = ({ songs }) => {
       {songs.map((song) => (
         <div key={song.id} className="flex items-center gap-x-4 w-full">
           <div className="flex-1">
-            <MediaItem onClick={() => {}} data={song} />
+            <MediaItem onClick={(id: string) => onPlay(id)} data={song} />
           </div>
           <LikeButton songId={song.id} />
         </div>
